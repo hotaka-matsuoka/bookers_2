322db634d705427eb204621796e656fa0c5e92d8
hotaka-matsuoka <matsuhota_mail108@icloud.com>
2020-07-02 18:08:05 +0900

    修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6b8fd22442d5e8d6b0481f0e747f090d161e9989ca9f8450eaf441ba40c323b04663e8975f247f2bdb25c3233c051a73e8523faa30bc694c9be7e165054dca3a'
